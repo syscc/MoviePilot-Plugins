1ed01068758dbb2542cdebc1aa7072d155fe0a03
so <so@Mac-mini.local>
2026-06-13 21:42:29 +0800

    Set message notify version to 1.0

--- plugins/messagenotify/__init__.py
+++ plugins/messagenotify/__init__.py
@@ -18,7 +18,7 @@ class MessageNotify(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/syscc/MoviePilot-Plugins/main/icons/messagenotify.png"
     # 插件版本
-    plugin_version = "0.0.1"
+    plugin_version = "1.0"
     # 插件作者
     plugin_author = "syscc"
     # 作者主页
